1f4497b45131125145b222054bd0ee4b5e471864
RaineW-Student <rwest1005@floridapoly.edu>
2025-08-22 15:40:54 +0000

    First program

---
+++ first_Lectures_stuff/first_program.c
@@ -0,0 +1,27 @@
+/*
+
+Multi-line comment.
+08/20/2025
+Igor Miralikhov
+The first program in C.
+
+*/
+
+// One line comment
+
+// Preprocessor directive 
+// stdio.h - standard input output
+// .h is a header file
+#include <stdio.h>
+
+// Create the main function
+int main(void) {
+    printf("Hello\n World\n"); // \n (new line) - escape sequence
+
+    printf("I\n am\n ready\n to\n code!\n");
+
+      printf("A\tb\tc\n");
+
+    return 0; // 0 means successful
+
+}
